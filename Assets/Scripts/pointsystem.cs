cca7c26d1052c8325d4e8c6875c0e1b2afb64cb7
MichaelIrlandez <m.a.isands2002@gmail.com>
2023-02-13 20:25:08 +1100

    reverted name changes

--- Assets/Scripts/pointsystem.cs
+++ Assets/Scripts/pointsystem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-public class PointSystem : MonoBehaviour
+public class pointsystem : MonoBehaviour
 {
     public TMP_Text scoreText;
     public int score;
