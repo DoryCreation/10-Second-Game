32bba5fb88a5270c5f23829ee2ed1a93bea61ea1
ollienye50 <olivianye50@gmail.com>
2023-02-13 14:04:11 +1100

    changed some layouts

---
+++ Assets/Scripts/collidersdetect.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collidersdetect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //detect when colliders are activated
+    //check if activated colliders are in a row/column
+    //if so, delete the objects that are in that row/colum and attached to it
+}
